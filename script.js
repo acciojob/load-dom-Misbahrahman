14b2b612c88075a577a0ecc4b5ac7d11af0c44c1
Misbahur rahman <98620184+Misbahrahman@users.noreply.github.com>
2023-12-22 20:02:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+window.addEventListener("load" , ()=>{
+    let ele = document.body;
+    let newEle = document.createElement("h1");
+    newEle.innerText = "DOM load success";
+    ele.appendChild(newEle);
+});
